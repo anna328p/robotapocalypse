508983485b1f7f3d44f7c97d50c7f321455bcc3a
Dmitry Kudriavtsev <me@dk0.us>
2018-11-16 01:59:56 -0800

    literally redid the whole thing lmao

--- game.py
+++ game.py
@@ -18,56 +18,8 @@ class Direction(Enum):
     LEFT = 3
 
 
-class Object(sprite.Sprite):
-    def __init__(self, groups, xpos, ypos, img):
-        super().__init__(groups)
-        self.xpos = xpos
-        self.ypos = ypos
-        self.image = img
-        self.rect = self.img.get_rect()
-        self.xvel = 0
-        self.yvel = 0
-
-    def tick(self):
-        """Ticks the object"""
-        self.rect = self.rect.move([self.xvel, self.yvel])
-
-
-class Player(Object):
-    def __init__(self, groups, img, accel, friction, top_speed, jump_limit, jump_velocity)
-        super().__init__(groups, 0, 0, img)
-        self.accel = accel
-        self.friction = friction
-        self.top_speed = top_speed
-        self.jump_limit = jump_limit
-        self.jump_velocity = jump_velocity
-        self.jumps = 0
-        self.movements = {"left": False, "right": False, "up": False}  # keep track of whether we are moving and where
-
-    def tick(self):
-        # ...
-        if True not in [self.movements['right'], self.movements['left']]:
-            xvel -= xvel * friction  # apply friction if the player is not moving
-
-        if self.movements['up'] and self.jumps < self.jump_limit:
-            self.movements['up'] = False
-            self.yvel = self.jump_velocity
-            self.jumps += 1
-
-        if self.movements['right']:
-            if self.xvel >= self.top_speed:
-                self.xvel = self.top_speed
-            else:
-                self.xvel += self.accel
-
-        if self.movements['left']:
-            if self.xvel <= -self.top_speed:
-                self.xvel = -self.top_speed
-            else:
-                self.xvel -= self.accel
-
-        self.yvel += gravity
-        super().tick()
+friction = 0.2
+gravity = 3/4
 
 
 def sign(value):
@@ -75,24 +27,132 @@ def sign(value):
     return 1 if value == abs(value) else -1
 
 
+class Object(sprite.Sprite):
+    def __init__(self, groups, xpos, ypos, img):
+        super().__init__(groups)
+        self.xpos = xpos
+        self.ypos = ypos
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.xvel = 0
+        self.yvel = 0
+        self.collisions = {
+            Direction.LEFT: False,
+            Direction.RIGHT: False,
+            Direction.UP: False,
+            Direction.DOWN: False
+        }
+
+    def update(self):
+        """Ticks the object"""
+        self.rect = self.rect.move([self.xvel, self.yvel])
+        super().update()
+
+    def is_colliding(self, crect, direction):
+        """Detects external collisions with a rect"""
+        if direction == Direction.UP:
+            return self.rect.top < crect.bottom
+        elif direction == Direction.LEFT:
+            return self.rect.left < crect.right
+        elif direction == Direction.RIGHT:
+            return self.rect.right > crect.left
+        elif direction == Direction.DOWN:
+            return self.rect.bottom > crect.top
+
+    def collide_internal(self, crect, direction):
+        """Detects internal collisions with a rect"""
+        if direction == Direction.UP:
+            return self.rect.top < crect.top
+        elif direction == Direction.LEFT:
+            return self.rect.left < crect.left
+        elif direction == Direction.RIGHT:
+            return self.rect.right > crect.right
+        elif direction == Direction.DOWN:
+            return self.rect.bottom > crect.bottom
+
+
+class Player(Object):
+    def __init__(self, groups, img, accel, top_speed, jump_limit, jump_velocity):
+        super().__init__(groups, 0, 0, img)
+        self.accel = accel
+        self.top_speed = top_speed
+        self.jump_limit = jump_limit
+        self.jump_velocity = jump_velocity
+        self.jumps = 0
+        self.movements = {
+            Direction.LEFT: False,
+            Direction.RIGHT: False,
+            Direction.UP: False
+        }  # keep track of whether we are moving and where
+
+    def collision(self):
+        pass
+
+    def update(self):
+        # ...
+        if True not in [self.movements[Direction.RIGHT], self.movements[Direction.LEFT]]:
+            self.xvel -= self.xvel * friction  # apply friction if the player is not moving
+
+        if self.movements[Direction.RIGHT]:
+            self.xvel += self.accel
+
+        if self.movements[Direction.LEFT]:
+            self.xvel -= self.accel
+
+        if abs(self.xvel) > self.top_speed:
+            self.xvel = sign(self.xvel) * self.top_speed
+
+        if self.collide_internal(bounds, Direction.LEFT):
+            self.rect = self.rect.move([-self.xvel + 1, 0])
+        elif self.collide_internal(bounds, Direction.RIGHT):
+            self.rect = self.rect.move([-self.xvel - 1, 0])
+
+        if self.collide_internal(bounds, Direction.UP):
+            self.yvel = gravity  # fix getting stuck on top of the window
+            self.rect = self.rect.move([0, -self.rect.top])  # fix flying above the window
+
+        if self.collide_internal(bounds, Direction.DOWN):
+            self.yvel = abs(self.yvel) * -bounciness
+            self.jumps = 0
+
+        if self.movements[Direction.UP] and self.jumps < self.jump_limit:
+            self.movements[Direction.UP] = False
+            self.yvel = self.jump_velocity
+            self.jumps += 1
+
+        self.yvel += gravity
+        super().update()
+
+
+bounciness = 0  # bounciness of the level floor
+friction = 0.2  # friction of the level floor
+
+bounds = []
+solids = []
+
 def main():
+    global bounds
+    global solids
+
     """Runs the game main loop"""
     pygame.init()
 
     size = width, height = 900, 600   # window size
-    fps = 120  # target fps which determines general game speed
+    fps = 75  # target fps which determines general game speed
     gravity = 3/4  # gravity constant
     white = 255, 255, 255  # background color
-    bounciness = 0  # bounciness of the level floor
-    friction = 0.2  # friction of the level floor
 
     screen = pygame.display.set_mode(size)
 
-    players = sprite.RenderClear()
+    drawgroup = sprite.RenderClear()
+    collidegroup = sprite.RenderClear()
+
+    bounds = screen.get_rect()
+    solids = collidegroup
 
     char = Player(
-        groups = players,
-        img = pygame.image.load("assets/pixelguy@2x.png"),  # the character sprite
+        groups = drawgroup,
+        img = pygame.image.load("assets/Walking2@3x.png"),  # the character sprite
         accel = 1,
         top_speed = 8,
         jump_limit = 2,
@@ -100,6 +160,11 @@ def main():
     )
 
     background = pygame.image.load("assets/Background@6x.png")
+    movements = {
+        Direction.LEFT: False,
+        Direction.RIGHT: False,
+        Direction.UP: False
+    }  # keep track of whether we are moving and where
 
     while True:
         for game_event in pygame.event.get():  # event processing
@@ -109,39 +174,28 @@ def main():
             if game_event.type == KEYDOWN:  # handle key press down
                 k = game_event.key  # code of the key being pressed down
                 if k == K_SPACE or k == K_UP:  # jump
-                    movements['up'] = True
+                    movements[Direction.UP] = True
                 if k == K_LEFT:  # move left
-                    movements['left'] = True
+                    movements[Direction.LEFT] = True
                 if k == K_RIGHT:  # move right
-                    movements['right'] = True
+                    movements[Direction.RIGHT] = True
             if game_event.type == KEYUP:  # handle key release
                 k = game_event.key  # code of the key being released
                 if k == K_LEFT:  # stop moving left
-                    movements['left'] = False
+                    movements[Direction.LEFT] = False
                 if k == K_RIGHT:  # stop moving right
-                    movements['right'] = False
+                    movements[Direction.RIGHT] = False
 
-        if char_rect.left < 0 or char_rect.right > width:
-            char_rect = char_rect.move([-velocity_x, 0])  # get stopped by window sides
+        char.movements = movements
 
-        if char_rect.top < 0:
-            velocity_y = gravity  # fix getting stuck on top of the window
-            char_rect = char_rect.move([0, -char_rect.top])  # fix flying above the window
+        drawgroup.update()
 
-
-        if char_rect.bottom > height:
-            if abs(velocity_y) > 0.5:
-                print("Bounced: %f" % velocity_y)
-            velocity_y = abs(velocity_y) * -bounciness
-            # velocity_y = -1
-            jumps = 0
-
-        screen.fill(white)
         screen.blit(background, (0, 0))
+        drawgroup.clear(screen, background)
+        drawgroup.draw(screen)
         pygame.display.flip()
 
-#        pygame.time.Clock().tick(fps)
-        sleep(1/fps)
+        pygame.time.Clock().tick(fps)
 
 
 if __name__ == "__main__":
